8997006bba08329d909ee7ab8f10faa7716e513d
Mulongovictor <mulongovibu@gmail.com>
2022-07-26 23:35:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# School Management
+
+This is a react project for a school management web application.
+
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import { SchoolManagement } from './components/SchoolManagement';
+
+function App() {
+  return (
+    <div className="App">
+      <SchoolManagement />
+    </div>
+  );
+}
+
+export default App;
